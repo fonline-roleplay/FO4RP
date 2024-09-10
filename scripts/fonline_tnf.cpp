c0cb437b5bc75112cd70e56c1d74177435dabfb2
git <APAmk2@users.noreply.github.com>
2024-09-10 15:27:14 +0400

    [Tech] SPECIAL Stat maximum increased to 15
    Still max 10 on registration.
    Added checks to traits, not allowing to register characters if special param buffed more than 10 (because server will clamp stats after registration).

--- scripts/fonline_tnf.cpp
+++ scripts/fonline_tnf.cpp
@@ -327,10 +327,12 @@ FONLINE_DLL_ENTRY( isCompiler )
 /* Parameters Get behaviors                                             */
 /************************************************************************/
 
+#define STAT_MAX_VAL	15
+
 EXPORT int getParam_Strength( CritterMutual& cr, uint )
 {
     int val = cr.Params[ ST_STRENGTH ] + cr.Params[ ST_STRENGTH_EXT ];
-    return CLAMP( val, 1, 10 );
+    return CLAMP( val, 1, STAT_MAX_VAL );
 }
 
 EXPORT int getParam_Perception( CritterMutual& cr, uint )
@@ -338,19 +340,19 @@ EXPORT int getParam_Perception( CritterMutual& cr, uint )
     int val = ( cr.Params[ DAMAGE_EYE ] ? 1 : cr.Params[ ST_PERCEPTION ] + cr.Params[ ST_PERCEPTION_EXT ] );
     if( cr.Params[ TRAIT_NIGHT_PERSON ] )
         val += GetNightPersonBonus();
-    return CLAMP( val, 1, 10 );
+    return CLAMP( val, 1, STAT_MAX_VAL );
 }
 
 EXPORT int getParam_Endurance( CritterMutual& cr, uint )
 {
     int val = cr.Params[ ST_ENDURANCE ] + cr.Params[ ST_ENDURANCE_EXT ];
-    return CLAMP( val, 1, 10 );
+    return CLAMP( val, 1, STAT_MAX_VAL );
 }
 
 EXPORT int getParam_Charisma( CritterMutual& cr, uint )
 {
     int val = cr.Params[ ST_CHARISMA ] + cr.Params[ ST_CHARISMA_EXT ];
-    return CLAMP( val, 1, 10 );
+    return CLAMP( val, 1, STAT_MAX_VAL );
 }
 
 EXPORT int getParam_Intellegence( CritterMutual& cr, uint )
@@ -358,19 +360,19 @@ EXPORT int getParam_Intellegence( CritterMutual& cr, uint )
     int val = cr.Params[ ST_INTELLECT ] + cr.Params[ ST_INTELLECT_EXT ];
     if( cr.Params[ TRAIT_NIGHT_PERSON ] )
         val += GetNightPersonBonus();
-    return CLAMP( val, 1, 10 );
+    return CLAMP( val, 1, STAT_MAX_VAL );
 }
 
 EXPORT int getParam_Agility( CritterMutual& cr, uint )
 {
     int val = cr.Params[ ST_AGILITY ] + cr.Params[ ST_AGILITY_EXT ];
-    return CLAMP( val, 1, 10 );
+    return CLAMP( val, 1, STAT_MAX_VAL );
 }
 
 EXPORT int getParam_Luck( CritterMutual& cr, uint )
 {
     int val = cr.Params[ ST_LUCK ] + cr.Params[ ST_LUCK_EXT ];
-    return CLAMP( val, 1, 10 );
+    return CLAMP( val, 1, STAT_MAX_VAL );
 }
 
 EXPORT int getParam_Hp( CritterMutual& cr, uint )
