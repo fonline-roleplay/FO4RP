fac785285e318d29b8fd16404a9c7fafee993a9b
APAmk2 <107351397+APAmk2@users.noreply.github.com>
2026-07-23 17:54:01 +0400

    [Tech] Updated `make_tnf.bat`
    Just set the fucking VS_PATH var, instead of replacing it's entries in bat

--- scripts/make_tnf.bat
+++ scripts/make_tnf.bat
@@ -3,17 +3,22 @@
 :1
 
 @: Environment
-@set PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE;%PATH%
-@set PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\BIN;%PATH%
-@set LIB=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\LIB;%LIB%
+@echo Setting up EnvVars...
+
+@set VS_PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0
+
+@set PATH=%VS_PATH%\Common7\IDE;%PATH%
+@set PATH=%VS_PATH%\VC\BIN;%PATH%
+@set LIB=%VS_PATH%\VC\LIB;%LIB%
 @set LIB=C:\Program Files (x86)\Microsoft SDKs\Windows\v6.0A\Lib;%LIB%
 @set LIB=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Lib;%LIB%
 @set LIB=.\StlPort;%LIB%
-@set INCLUDE=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\include;%INCLUDE%
+@set INCLUDE=%VS_PATH%\VC\include;%INCLUDE%
 @set INCLUDE=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Include;%INCLUDE%
 @set INCLUDE=.\StlPort;%INCLUDE%
 
 @: Server
+@echo Building Server lib...
 @del ".\\fonline_tnf.dll"
 cl.exe /nologo /MT /W3 /O2 /Gd /D "__SERVER" /Fd".\\fonline_tnf.obj" /FD /c .\\fonline_tnf.cpp qmap_tools.cpp
 link.exe /nologo /dll /incremental:no /machine:I386 .\\fonline_tnf.obj qmap_tools.obj /out:".\\fonline_tnf.dll"
@@ -27,6 +32,7 @@ link.exe /nologo /dll /incremental:no /machine:I386 .\\fonline_tnf.obj qmap_tool
 @del ".\\qmap_tools.obj"
 
 @: Client
+@echo Building Client lib...
 
 cl.exe /nologo /MT /W3 /O2 /Gd /D "__CLIENT" /Fd".\\fonline_tnf.obj" /FD /c .\\fonline_tnf.cpp qmap_tools.cpp q_sprites.cpp
 link.exe /nologo /dll /incremental:no /machine:I386 .\\fonline_tnf.obj qmap_tools.obj q_sprites.obj /out:".\\fonline_tnf_client.dll"
